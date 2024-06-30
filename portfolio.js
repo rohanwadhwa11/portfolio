ba0ac9699e61ae616846d7975d023e883d2a6122
rohan wadhwa <rohanwadhwa913@outlook.com>
2024-06-30 18:54:01 +0530

    new features

---
+++ portfolio.js
@@ -0,0 +1,63 @@
+let sideMenu = document.querySelector(".side-menu");
+let frontLine = document.querySelector(".topLine");
+let middleLine = document.querySelector(".middleLine");
+let lastLine = document.querySelector(".lastLine");
+let dropboxNav = document.querySelector(".head-dropbox");
+
+sideMenu.addEventListener("click",() => {
+    frontLine.classList.toggle("top-cross");
+    middleLine.classList.toggle("bottom-cross");
+    for(list of frontLine.classList){
+        if(list == "top-cross"){
+            lastLine.style.display = "none";
+            dropboxNav.style.display = "block";
+        }
+        else{
+        lastLine.style.display = "flex";
+        dropboxNav.style.display = "none";
+        }
+    }
+    console.log("hello");
+})
+
+let colapse = document.querySelectorAll(".section-block h3");
+let downIcon = document.querySelectorAll(".section-block h3 i");
+console.log(colapse);
+/* console.log(downIcon);
+console.log(downIcon[0].parentElement); */
+for(let col of colapse){
+
+col.addEventListener("click",function() {
+console.log(this);
+this.classList.add("colapsed");
+
+for(col of colapse){
+    if(col == this)
+        continue;
+    else{
+        if(col.classList == "colapsed"){
+            let content = col.nextElementSibling;
+            if(content.style.display == "block")
+                content.style.display = "none";
+        }
+        /* console.log(col.classList); */
+    }
+}
+
+let content = this.nextElementSibling;
+if(content.style.display == "block"){
+    content.style.display = "none";
+    this.style.backgroundColor = "black";
+    this.style.color = "white";
+}
+else{
+    content.style.display = "block";
+    this.style.backgroundColor = "grey";
+    this.style.color = "black";
+}
+    
+let downIcon = this.children;
+downIcon[0].classList.toggle("fa-caret-down");
+/* console.log(downIcon[0].classList); */
+})
+}
